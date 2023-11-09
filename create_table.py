aa4b1e8aa5dc088d79d3babfe949fdadd8df3b6d
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2023-11-09 16:52:20 -0300

    criando novas tabelas no bd clinica_vision

--- create_table.py
+++ create_table.py
@@ -1,3 +1,40 @@
+#!/usr/bin/python
+
+import psycopg2
+from config import config
 
 
-def create_table()
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE pacientes (
+            paciente_id SERIAL PRIMARY KEY,
+            nome VARCHAR(50) UNIQUE NOT NULL,
+            telefone INT 
+        )
+        """,
+   )
+    
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    create_tables()
